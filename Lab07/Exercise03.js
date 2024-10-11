91ad0fdbec6b7b519a33f7b608b678f049e904f5
thuynh-miu <thuynh@miu.edu>
2024-10-11 14:39:47 -0500

    print only names of recipes

--- Lab07/Exercise03.js
+++ Lab07/Exercise03.js
@@ -3,7 +3,7 @@
         let response = await fetch('https://dummyjson.com/recipes')
         if (response.ok) {
             let json = await response.json()
-            console.log(json)
+            console.log(json['recipes'].map(r => r.name))
         }
         else {
             console.log("HTTP Error: " + response.status)
