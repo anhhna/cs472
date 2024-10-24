d7859fa7c0f71c22d231ab08aad0becb7dfdb87e
thuynh-miu <thuynh@miu.edu>
2024-10-23 22:47:30 -0500

    redirect / to /api-docs

--- project/server/app.js
+++ project/server/app.js
@@ -29,19 +29,25 @@ const swaggerOptions = {
     apis: ['./route/*.js']
 }
 
-// Generate Swagger specification
+// Swagger specification
 const swaggerSpec = swaggerJsdoc(swaggerOptions)
 
-// Use Swagger UI middleware
+// Redirect '/' to '/api-docs'
+app.get('/', (req, res) => {
+    res.redirect('/api-docs')
+})
+
+// Swagger setup
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.use('/api/v1/dictionary', dictionaryRouter)
 
-// Error handling
+
 app.use((req, res, next) => {
     res.status(404).json({ error: req.url + ' API not supported' })
 })
 
+// Error handler
 app.use((err, req, res, next) => {
     if (err.message === 'NOT Found')
         res.status(404).json({ error: err.message })
